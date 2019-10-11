7abb778d36ee163f5682d2ded158c7428031e00d
Material Fail <material.fail@gmail.com>
2019-10-11 10:38:12 -0300

    new layout

--- js/js.js
+++ js/js.js
@@ -2,26 +2,6 @@
 layout: none  
 ---
 
-{% assign totalBackgrounds = 0 %}
-{% for file in site.static_files %}
-  {% if file.path contains "/backgrounds/" %}
-    {% assign totalBackgrounds = totalBackgrounds | plus: 1 %}
-  {% endif %}
-{% endfor %}
-
-function getDayOfYear() {
-  const now = new Date();
-  const start = new Date(now.getFullYear(), 0);
-  const diff = now - start;
-  const oneDayMillis = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDayMillis);
-  return dayOfYear + window.location.href.length;
-}
-
-function getBackgroundImageIndex() {
-   return getDayOfYear() % {{ totalBackgrounds }};
- }
-
 function clearLanguageSelection() {
   const langSelectorDivs = document.getElementsByClassName('language-selector');
   const infoDivs = document.getElementsByClassName('info-content');
@@ -48,11 +28,5 @@ window.onload = function() {
   for(let i = 0; i < langSelectorDivs.length; i++) {
     langSelectorDivs[i].onclick = selectThisLanguage;
     langSelectorDivs[i].click();
-  }
-
-  // set background image
-  const bgDiv = document.getElementById('daily-image');
-  if(bgDiv) {
-    bgDiv.style['background-image'] = `url({{ site.baseurl }}/media/images/backgrounds/background-${getBackgroundImageIndex()}.jpg)`;
   }
 }
