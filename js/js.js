b544a78313224185c323537785084737878acdf8
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-05-02 16:04:37 -0300

    cycle background images

--- js/js.js
+++ js/js.js
@@ -22,11 +22,31 @@ function selectThisLanguage() {
   infoDiv.style.display = 'block';
 }
 
+function dayOfYear() {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = now - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay);
+  return day;
+}
+
+function setBackgroundImage() {
+  const backgroundImageElement = document.getElementById('the-background-image');
+  const urlLength = window.location.href.replace(/\//g, '').length;
+  const dailyRandomNumber = (urlLength + dayOfYear()) % 10;
+  const newImageUrl = `url({{ site.baseurl }}/media/images/backgrounds/background-sq-${dailyRandomNumber}.jpg)`;
+  console.log(newImageUrl);
+  console.log(backgroundImageElement.style);
+  backgroundImageElement.style['background-image'] = newImageUrl;
+}
+
 window.onload = function() {
-  // load onclicks
   const langSelectorDivs = document.getElementsByClassName('language-selector');
   for(let i = 0; i < langSelectorDivs.length; i++) {
     langSelectorDivs[i].onclick = selectThisLanguage;
     langSelectorDivs[i].click();
   }
+
+  setBackgroundImage();
 }
